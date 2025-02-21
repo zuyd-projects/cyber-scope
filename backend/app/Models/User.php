f1f8b7de187a8fa8bda99de7d77cfdaf50fe9a71
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-02-21 10:57:51 +0100

    feat(backend): Added Laravel Breeze (auth scaffolding)

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use \Laravel\Sanctum\HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
