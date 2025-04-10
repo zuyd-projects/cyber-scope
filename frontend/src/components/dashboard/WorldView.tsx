e950950c299686eb5637853ab725e6f7788a2467
Underlyingglitch <36314703+Underlyingglitch@users.noreply.github.com>
2025-04-10 13:18:44 +0200

    fix(frontend): set globe URL

--- frontend/src/components/dashboard/WorldView.tsx
+++ frontend/src/components/dashboard/WorldView.tsx
@@ -5,7 +5,7 @@ export function WorldView() {
     <div className="flex flex-col flex-1">
       <div className="w-full h-[50vh] rounded-xl overflow-hidden shadow-lg">
         <iframe
-          src="http://localhost:5173/"
+          src="https://globe-cyberscope.rickokkersen.nl/"
           className="w-full h-full rounded-xl border border-gray-200"
           title="World Globe"
           allowFullScreen
