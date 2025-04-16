9907724c2a22ca8bc46da5c1e170f55b479e5971
Max Miguel <86264109+Prism098@users.noreply.github.com>
2025-04-16 13:15:23 +0200

    feat(dashboard): add ip2 tag

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
       <h1 className="text-2xl font-semibold mb-2">You are being redirected...</h1>
       <p className="text-lg text-muted-foreground">Greetings from the CyberScope team 👋</p>
+      <span>CyberScope uses the IP2Location LITE database for <a href="https://lite.ip2location.com">IP geolocation</a>.</span>
     </div>
   );
 }
